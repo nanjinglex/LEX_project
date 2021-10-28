84f75748ab3fafc50a809535f4fc57e4b7de5050
刘二兴 <lexliu@tungee.com>
2021-10-28 20:19:00 +0800

    多模块首次提交

---
+++ common/src/main/java/org/example/common/constants/AlarmConstant.java
@@ -0,0 +1,10 @@
+package org.example.common.constants;
+
+/**
+ * @Author Ken
+ * 告警相关变量
+ */
+public class AlarmConstant {
+    public static final String ALARM_KEY_JOB_ERROR = "JOB_ERROR";
+    public static final String ALARM_KEY_JOB_FINISH = "JOB_FINISH";
+}
---
+++ product/src/main/java/com/example/product/CommonTest.java
@@ -0,0 +1,20 @@
+package com.example.product;
+
+import org.example.common.constants.AlarmConstant;
+
+/**
+ * @author lex
+ * @version 1.0.0
+ * @ClassName CommonTest.java
+ * @Description TODO
+ * @createTime 2021年10月28日 15:39:00
+ */
+public class CommonTest {
+    public static void main(String[] args) {
+        CommonTest.test();
+    }
+
+    public static void test() {
+        System.out.println(AlarmConstant.ALARM_KEY_JOB_ERROR);
+    }
+}
